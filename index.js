ce78b78f94388cca839917073cbcc17a79abf09f
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-14 04:32:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+  
+  variable uppercase 
   if(string.toUpperCase()){
      return "YES INDEED!"
   } else if(string.toLowerCase()){
-     return "I can't hear you!"
+     return "I can\'t hear you!"
   }else if(string === "I love you, Grandma."){
      return "I love you, too."
     
